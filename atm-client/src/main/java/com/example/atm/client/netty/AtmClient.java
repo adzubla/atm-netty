a6bd4f29ec3404a4da150827fe43c88cd4dfad7f
Eduardo Ito <eduardo.ito@gmail.com>
2020-06-17 18:08:57 -0300

    Refactor

--- atm-client/src/main/java/com/example/atm/client/netty/AtmClient.java
+++ atm-client/src/main/java/com/example/atm/client/netty/AtmClient.java
@@ -1,11 +1,20 @@
 package com.example.atm.client.netty;
 
+import com.example.atm.netty.codec.atm.AtmDecoder;
+import com.example.atm.netty.codec.atm.AtmEncoder;
 import com.example.atm.netty.codec.atm.AtmMessage;
+import com.example.atm.netty.codec.crypto.CryptoDecoder;
+import com.example.atm.netty.codec.crypto.CryptoEncoder;
+import com.example.atm.netty.codec.header.HeaderDecoder;
+import com.example.atm.netty.codec.header.HeaderEncoder;
+import com.example.atm.netty.codec.length.LengthFrameDecoder;
+import com.example.atm.netty.codec.length.LengthPrepender;
+import com.example.atm.netty.codec.mac.MacDecoder;
+import com.example.atm.netty.codec.mac.MacEncoder;
 import io.netty.bootstrap.Bootstrap;
-import io.netty.channel.Channel;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.EventLoopGroup;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 
 public final class AtmClient {
@@ -26,7 +35,26 @@ public final class AtmClient {
         Bootstrap b = new Bootstrap();
         b.group(group)
                 .channel(NioSocketChannel.class)
-                .handler(new AtmClientInitializer());
+                .handler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel ch) {
+                        ChannelPipeline pipeline = ch.pipeline();
+
+                        pipeline.addLast(new LengthFrameDecoder());
+                        pipeline.addLast(new CryptoDecoder());
+                        pipeline.addLast(new MacDecoder());
+                        pipeline.addLast(new HeaderDecoder());
+                        pipeline.addLast(new AtmDecoder());
+
+                        pipeline.addLast(new LengthPrepender());
+                        pipeline.addLast(new CryptoEncoder());
+                        pipeline.addLast(new MacEncoder());
+                        pipeline.addLast(new HeaderEncoder());
+                        pipeline.addLast(new AtmEncoder());
+
+                        pipeline.addLast(new AtmClientHandler());
+                    }
+                });
 
         channel = b.connect(host, port).sync().channel();
     }
--- atm-server/src/main/java/com/example/atm/server/netty/AtmServer.java
+++ atm-server/src/main/java/com/example/atm/server/netty/AtmServer.java
@@ -1,8 +1,21 @@
 package com.example.atm.server.netty;
 
+import com.example.atm.netty.codec.atm.AtmDecoder;
+import com.example.atm.netty.codec.atm.AtmEncoder;
+import com.example.atm.netty.codec.crypto.CryptoDecoder;
+import com.example.atm.netty.codec.crypto.CryptoEncoder;
+import com.example.atm.netty.codec.header.HeaderDecoder;
+import com.example.atm.netty.codec.header.HeaderEncoder;
+import com.example.atm.netty.codec.length.LengthFrameDecoder;
+import com.example.atm.netty.codec.length.LengthPrepender;
+import com.example.atm.netty.codec.mac.MacDecoder;
+import com.example.atm.netty.codec.mac.MacEncoder;
 import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
@@ -28,7 +41,26 @@ public final class AtmServer {
         b.group(bossGroup, workerGroup)
                 .channel(NioServerSocketChannel.class)
                 .handler(new LoggingHandler(LogLevel.INFO))
-                .childHandler(new AtmServerInitializer(listener));
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel ch) {
+                        ChannelPipeline pipeline = ch.pipeline();
+
+                        pipeline.addLast(new LengthFrameDecoder());
+                        pipeline.addLast(new CryptoDecoder());
+                        pipeline.addLast(new MacDecoder());
+                        pipeline.addLast(new HeaderDecoder());
+                        pipeline.addLast(new AtmDecoder());
+
+                        pipeline.addLast(new LengthPrepender());
+                        pipeline.addLast(new CryptoEncoder());
+                        pipeline.addLast(new MacEncoder());
+                        pipeline.addLast(new HeaderEncoder());
+                        pipeline.addLast(new AtmEncoder());
+
+                        pipeline.addLast(new AtmServerHandler(listener));
+                    }
+                });
 
         b.bind(port).sync().await();
     }
