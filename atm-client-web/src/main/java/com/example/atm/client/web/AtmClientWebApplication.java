11de4dcc98aa7a98fa2ba38cbc48f19a95274e4e
Eduardo Ito <eduardo.ito@gmail.com>
2020-08-17 19:35:20 -0300

    Modulo atm-client-web

---
+++ atm-client-web/src/main/java/com/example/atm/client/web/AtmClientWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.atm.client.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtmClientWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtmClientWebApplication.class, args);
+    }
+
+}
