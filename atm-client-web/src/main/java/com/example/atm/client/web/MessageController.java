16ecab0070df8057b9f9d1709cd583f85d4353ea
Eduardo Ito <eduardo.ito@gmail.com>
2020-08-19 18:17:28 -0300

    Modulo atm-client-web

--- atm-client-web/src/main/java/com/example/atm/client/web/MessageController.java
+++ atm-client-web/src/main/java/com/example/atm/client/web/MessageController.java
@@ -1,36 +1,67 @@
 package com.example.atm.client.web;
 
+import com.example.atm.client.netty.AtmClient;
+import com.example.atm.netty.codec.atm.AtmMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.event.EventListener;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.socket.messaging.SessionConnectEvent;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
-import org.springframework.web.util.HtmlUtils;
+
+import java.math.BigInteger;
 
 @Controller
 public class MessageController {
+    private static final Logger LOG = LoggerFactory.getLogger(MessageController.class);
+
+    @Autowired
+    private SimpMessagingTemplate template;
+
+    @Value("${atm-server.host}")
+    private String host;
+
+    @Value("${atm-server.port}")
+    private int port;
+
+    private AtmClient client;
 
     @EventListener
-    public void handleSessionConnected(SessionConnectEvent event) {
-        System.out.println("*** Connect");
+    public void handleSessionConnected(SessionConnectEvent event) throws InterruptedException {
+        LOG.info("*** Connect");
         SimpMessageHeaderAccessor headers = SimpMessageHeaderAccessor.wrap(event.getMessage());
-        System.out.println("headers = " + headers);
+        LOG.debug("headers = " + headers);
+
+        client = new AtmClient(host, port, new WebAtmClientHandler(template));
+        client.connect();
     }
 
     @EventListener
     public void handleSessionDisconnect(SessionDisconnectEvent event) {
-        System.out.println("*** Disconnect");
+        LOG.info("*** Disconnect");
+        client.close();
     }
 
     @MessageMapping("/receive/{atmId}")
-    @SendTo("/topic/response/{atmId}")
-    public ResponseMessage receive(ReceiveMessage message, @DestinationVariable Long atmId) throws Exception {
-        System.out.println("*** Received from " + atmId + ": " + message.getName());
-        String response = message.getName().toUpperCase();
-        return new ResponseMessage(HtmlUtils.htmlEscape(response));
+    public void receive(ReceiveMessage message, @DestinationVariable Long atmId) throws Exception {
+        LOG.debug("*** Received from " + atmId + ": " + message.getName());
+
+        AtmMessage atmMessage = new AtmMessage(atmId, createIsoMessage(message.getName()));
+        client.write(atmMessage, true);
+    }
+
+    private String createIsoMessage(String line) {
+        String mti = "0100";
+        StringBuilder bitmap = new StringBuilder("0010001000010000000000000001000100000010110000000100100000000101");
+        String bitmapHex = (new BigInteger(bitmap.toString(), 2)).toString(16).toUpperCase();
+        assert bitmapHex.length() == 16;
+        return mti + bitmapHex + line;
     }
 
 }
---
+++ atm-client-web/src/main/java/com/example/atm/client/web/WebAtmClientHandler.java
@@ -0,0 +1,57 @@
+package com.example.atm.client.web;
+
+import com.example.atm.client.netty.AtmClientHandler;
+import com.example.atm.netty.codec.atm.AtmMessage;
+import com.example.atm.netty.codec.header.HeaderData;
+import io.netty.channel.ChannelHandlerContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.util.HtmlUtils;
+
+public class WebAtmClientHandler extends AtmClientHandler {
+    private static final Logger LOG = LoggerFactory.getLogger(WebAtmClientHandler.class);
+
+    private final SimpMessagingTemplate template;
+
+    public WebAtmClientHandler(SimpMessagingTemplate template) {
+        this.template = template;
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) {
+        LOG.debug("Connected: " + ctx.channel());
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) {
+        LOG.debug("Disconnected: " + ctx.channel());
+    }
+
+    @Override
+    public void channelRead0(ChannelHandlerContext ctx, AtmMessage msg) {
+        Byte type = ctx.channel().attr(HeaderData.HEADER_TYPE_ATTRIBUTE_KEY).get();
+        String response;
+        if (type == HeaderData.PONG) {
+            response = String.format("[PONG] %s", msg.getBody());
+        } else {
+            String body = msg.getBody();
+
+            String mti = body.substring(0, 4);
+            String bitmap = body.substring(4, 20);
+            String text = body.substring(20);
+
+            response = String.format("[%s,%s] %s%n", mti, bitmap, text);
+        }
+
+        LOG.debug("Response: {}", response);
+        template.convertAndSend("/topic/response/" + msg.getId(), new ResponseMessage(HtmlUtils.htmlEscape(response)));
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+}
